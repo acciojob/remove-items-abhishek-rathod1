e85c6e584d52847da784d6fea328c4890f5272c6
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-20 10:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let colorSet = document.getElementById("colorSelect");
+// 
+// 
+function removeColor(){
+	
+let selectedColor = colorSet.options[colorSet.selectedIndex].value
+console.log(selectedColor);
+	colorSet.remove(selectedColor);	
+	
+}
